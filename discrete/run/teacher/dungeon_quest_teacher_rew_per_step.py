0693cba83b92177659d3bb3493732ea4290d6f93
adenm <aden.mckinney10@gmail.com>
2024-06-24 22:26:40 -0400

    fixed import error

--- discrete/run/teacher/dungeon_quest_teacher_rew_per_step.py
+++ discrete/run/teacher/dungeon_quest_teacher_rew_per_step.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 """
 
 from discrete.lib.main import run_training
-from discrete.run.env.dungeon_quest_7 import dungeon_quest_rew_per_step_env_config
+from discrete.run.env.dungeon_quest_7 import dungeon_quest_config_7
 from discrete.run.utils import teacher_config_v1
 
 device = torch.device("cpu")
@@ -30,7 +30,7 @@ else:
 
 
 config = teacher_config_v1(
-    dungeon_quest_rew_per_step_env_config,
+    dungeon_quest_config_7,
     "dungeon_quest_teacher_rew_per_step",
     device,
     max_training_steps=int(5e5))
