41285f9f78df110e013a4bcbd130a8f3e5ff2398
adenm <aden.mckinney10@gmail.com>
2024-06-27 09:10:06 -0400

    changed learning rate back to successful preliminary for DDPG actor

--- discrete/run/teacher/dungeon_quest_teacher_rew_per_step_7_productMDP.py
+++ discrete/run/teacher/dungeon_quest_teacher_rew_per_step_7_productMDP.py
@@ -16,7 +16,7 @@ else:
     print("No CUDA detected, using CPU...\n")
     # assert False
 
-max_training_steps=int(6e5)
+max_training_steps=int(1e6)
 
 # config = teacher_config_v1(dungeon_quest_rew_per_step_env_config_7, "dungeon_quest_teacher_rew_per_step_7_productMDP",
 #                            device, agent_cls=OneHotAutomatonAfterFeatureExtractorAgent, aps=dungeon_quest_aps,
