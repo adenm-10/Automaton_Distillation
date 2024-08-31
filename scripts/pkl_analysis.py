8123a3a1b71f720ae959670a712180d46224d6f2
adenm <aden.mckinney10@gmail.com>
2024-08-31 10:59:16 -0400

    updated reward test parameters

--- scripts/pkl_analysis.py
+++ scripts/pkl_analysis.py
@@ -30,46 +30,47 @@ if __name__ == "__main__":
 
 
     # Distillation data dictionary read
-    file_distill_name = "rew_and_steps_lists.pkl"
-    path_distill_to_out = "past_runs/363227"
-    file_distill_path = f"{path_distill_to_out}/{file_distill_name}"
-    with open(file_distill_path, 'rb') as file_distill_:
-        data = pickle.load(file_distill_)
+    # file_distill_name = "rew_and_steps_lists.pkl"
+    # path_distill_to_out = "test_output/369875/dragon-r_10_key_r_1_sword_i_1_shield_i_1_seq-level_2/rew_and_steps_lists.pkl"
+    # file_distill_path = f"{path_distill_to_out}/{file_distill_name}"
+    # with open(file_distill_path, 'rb') as file_distill_:
+    #     data = pickle.load(file_distill_)
 
-    data_distill = data
-    for key, value in data_distill.items():
-        data_distill[key] = moving_average(value)[0:x_length]
+    # data_distill = data
+    # for key, value in data_distill.items():
+    #     data_distill[key] = moving_average(value)[0:x_length]
         
     # Independent agent data dictionary read
     file_independent_name = "rew_and_steps_lists.pkl"
-    path_independent_to_out = "past_runs/363228/dragon-r_10_bound-persist_True_bounding-dist_3_seq-level__index_1"
+    path_independent_to_out = "test_output/369875/dragon-r_10_key_r_1_sword_i_1_shield_i_1_seq-level_2"
     file_independent_path = f"{path_independent_to_out}/{file_independent_name}"
     with open(file_independent_path, 'rb') as file:
         data = pickle.load(file)
 
     data_independent = data
     for key, value in data_independent.items():
+        print(f"Data points: {len(data[key])}")
         data_independent[key] = moving_average(value)[0:x_length]
 
     # Rewards per episode
-    plt.plot(x_axis_iterable, data_distill['rewards_per_episode'],   color='blue',  label='Distilled Agent')
+    # plt.plot(x_axis_iterable, data_distill['rewards_per_episode'],   color='blue',  label='Distilled Agent')
     plt.plot(x_axis_iterable, data_independent['rewards_per_episode'],   color='red',  label='Independent Agent')
     plt.xlabel('Episodes')
     plt.ylabel('Rewards per Episode')
     plt.legend(loc="upper right")
 
-    plt.savefig(f'{path_distill_to_out}/Student_Rewards_Comp_{x_length}_Episodes.png')
+    plt.savefig(f'{path_independent_to_out}/Student_Rewards_Comp_{x_length}_Episodes.png')
 
     plt.clf()
 
 
     # Steps to Terminal State per episode
-    plt.plot(x_axis_iterable, data_distill['steps_to_term_per_episode'], color='blue', label = 'Distilled Agent')
+    # plt.plot(x_axis_iterable, data_distill['steps_to_term_per_episode'], color='blue', label = 'Distilled Agent')
     plt.plot(x_axis_iterable, data_independent['steps_to_term_per_episode'], color='red', label = 'Independent Agent')
     plt.xlabel('Episodes')
     plt.ylabel('Steps to Terminal State')
     plt.legend(loc="upper right")
 
-    plt.savefig(f'{path_distill_to_out}/Student_Steps_Comp_{x_length}_Episodes.png')
+    plt.savefig(f'{path_independent_to_out}/Student_Steps_Comp_{x_length}_Episodes.png')
     
     plt.clf()
